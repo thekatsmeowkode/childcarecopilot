b2c07a88d9bcacbaf8b288d29485649b8238b200
Kat <105607428+thekatsmeowkode@users.noreply.github.com>
2023-07-31 20:25:27 -0400

    Add POST route for school data

--- backend/controllers/schoolController.js
+++ backend/controllers/schoolController.js
@@ -1,10 +1,8 @@
 const School = require("../models/SchoolModel");
-const mongoose = require("mongoose");
 
 //POST a school
 const addSchool = async (req, res) => {
   const { ...form } = req.body;
-  console.log(req.body);
   try {
     const school = await School.create({ ...form });
     res.status(200).json(school);
@@ -19,6 +17,16 @@ const getSchool = async (req, res) => {
   res.status(200).json(school);
 };
 
-const updateSchool = async (req, res) => {};
+const updateSchool = async (req, res) => {
+  await School.findByIdAndUpdate("64c84bb803361397826fd4fa", req.body),
+    function (err, docs) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Updated User: ", docs);
+      }
+    };
+  console.log(JSON.stringify(req.body));
+};
 
 module.exports = { addSchool, getSchool, updateSchool };
