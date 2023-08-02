f372db608cdd3ae2b2d9b45bcf5f126564af5e55
Kat <105607428+thekatsmeowkode@users.noreply.github.com>
2023-08-01 20:03:29 -0400

    Add functionality to get the number of teachers needed per room

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -4,6 +4,7 @@ import DashboardSquare from "../components/DashboardSquare";
 const Dashboard = () => {
   const [revenueDetails, setRevenueDetails] = useState(null);
   const [totalStudents, setTotalStudents] = useState(null);
+  const [staffCoreHours, setStaffCoreHours] = useState(null);
 
   const CLASSROOM_NAMES = {
     infants: "infants",
@@ -29,31 +30,37 @@ const Dashboard = () => {
       headers: { "Content-Type": "application/json" },
     });
     const json = await totalStudents.json();
-    console.log(json)
+    console.log(json);
     //json to get is json.classRevenue
     return json;
   };
 
-  const getStudentAges = async () => {
+  const getStaffCoreHours = async () => {
+    const coreStaff = await fetch("api/school/staff-required", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    const json = await coreStaff.json();
+    return json;
+  };
 
-  }
-  
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [revenueData, studentData] = await Promise.all([
+        const [revenueData, studentData, staffCoreData] = await Promise.all([
           getClassRevenue(),
           getTotalStudents(),
+          getStaffCoreHours(),
         ]);
         setRevenueDetails(revenueData);
         setTotalStudents(studentData);
+        setStaffCoreHours(staffCoreData);
       } catch (error) {
         console.error("Error fetching dashboard data", error);
       }
     };
-      fetchData()
-    }, [])
-
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -64,6 +71,11 @@ const Dashboard = () => {
       )}
       {totalStudents ? (
         <DashboardSquare squareDetails={totalStudents} />
+      ) : (
+        "Loading..."
+      )}
+      {staffCoreHours ? (
+        <DashboardSquare squareDetails={staffCoreHours} />
       ) : (
         "Loading..."
       )}
